df5fd566efa3d5166cb683259ced63c10ec04240
jethuan <hitdlu@gmail.com>
2021-11-13 10:31:10 +0700

    update state v2

---
+++ src/component/state.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+export default class Wcom extends Component {
+    constructor(props){
+        super(props){
+            this.state= {
+                text:'Hello'
+            }
+        }
+    }
+}
