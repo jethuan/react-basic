b5d89f3cd7bd7f76ee91c5842dedd3a29ea6af1e
jethuan <hitdlu@gmail.com>
2021-11-16 17:42:33 +0700

    ví du: dùng function thay class

--- src/view/example/ChildComponent.js
+++ src/view/example/ChildComponent.js
@@ -1,30 +1,51 @@
 import react, { Component } from "react";
 
-export default class ChildComponent extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    console.log(this.props);
-    let arr = this.props.arrStudent;
-    return (
-      <>
-        <div>
-          <p>tên tôi là : {this.props.name}</p>
-          <p>tên tôi là : {this.props.age}</p>
-          <p>
-            tên tôi là :{" "}
-            {arr.map((items, index) => {
-              return (
-                <div key={items.id}>
-                  {items.id} - {items.name} - {items.age}
-                </div>
-              );
-            })}
-          </p>
-        </div>
-        <div></div>
-      </>
-    );
-  }
-}
+// export default class ChildComponent extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     console.log(this.props);
+//     let arr = this.props.arrStudent;
+//     return (
+//       <>
+//         <div>
+//           <p>tên tôi là : {this.props.name}</p>
+//           <p>tên tôi là : {this.props.age}</p>
+//           <p>
+//             tên tôi là :{" "}
+//             {arr.map((items, index) => {
+//               return (
+//                 <div key={items.id}>
+//                   {items.id} - {items.name} - {items.age}
+//                 </div>
+//               );
+//             })}
+//           </p>
+//         </div>
+//         <div></div>
+//       </>
+//     );
+//   }
+// }
+
+// khai báo function thay vì dùng class. function chỉ nên dùng với hook
+
+const ChildComponent = (props) => {
+  console.log("In ra props", props);
+  let { arrStudent } = props;
+
+  return (
+    <div>
+      {arrStudent.map((items, index) => {
+        return (
+          <div key={items.id}>
+            {items.id} - {items.name} - {items.age}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ChildComponent;
